93db7ba66c46ce8a25b7191a4168e34917e60814
Nelson Florez <nelsonflorez11@gmail.com>
2022-09-26 22:29:00 -0500

    ADD: Issue #1 final 04

--- infrastructure/web/scripts/BootcampDevOps.sh
+++ infrastructure/web/scripts/BootcampDevOps.sh
@@ -4,7 +4,7 @@ sudo apt-get update -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
-sudo apt install docker-ce docker-ce-cli containerd.io -y
+sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 sudo addgroup docker
 sudo usermod -aG docker $USER
 sudo chmod 666 /var/run/docker.sock
